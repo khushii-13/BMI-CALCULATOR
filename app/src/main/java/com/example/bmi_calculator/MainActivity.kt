5759181885e6f5c6c506ebf30b7dac8961911305
KHUSHI CHAUDHARY <khushichaudhary904@gmail.com>
2024-01-07 01:52:03 +0530

    07-01-2024

--- app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.bmi_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var wt= findViewById<EditText>(R.id.enterWeight)
+        var ht = findViewById<EditText>(R.id.enterHeight)
+        var checkbtn=findViewById<Button>(R.id.bmiCalculate)
+        var answerid=findViewById<TextView>(R.id.answer)
+        checkbtn.setOnClickListener {
+
+            val w= wt.text.toString().toFloat()
+            var h = ht.text.toString().toFloat()
+            h /= 100
+            val ans= w/(h*h)
+            val bmi =String.format("%.2f",ans).toFloat()
+            answerid.text="Your Bmi is "+bmi.toString()
+
+
+
+        }
     }
 }
