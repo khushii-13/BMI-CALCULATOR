ea058934b68b0c81d0317bdb54f4d8568a4c701b
KHUSHI CHAUDHARY <khushichaudhary904@gmail.com>
2024-01-08 00:12:05 +0530

    08-01-2024

--- app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bmi_calculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,6 +8,7 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,15 +18,25 @@ class MainActivity : AppCompatActivity() {
         var answerid=findViewById<TextView>(R.id.answer)
         checkbtn.setOnClickListener {
 
-            val w= wt.text.toString().toFloat()
+            val w = wt.text.toString().toFloat()
             var h = ht.text.toString().toFloat()
             h /= 100
-            val ans= w/(h*h)
-            val bmi =String.format("%.2f",ans).toFloat()
-            answerid.text="Your Bmi is "+bmi.toString()
+            val ans = w/(h*h)
+            val bmi = String.format("%.2f",ans).toFloat()
+            answerid.text = display(ans)+bmi.toString()
+            wt.text = null
+            ht.text = null
 
+        }
+    }
+    private fun display(bmi: Float) :String{
 
-
+        if(bmi < 18.5){
+            return   "UnderWeight BMI :"
+        }else if(bmi in 18.5..24.9){
+            return  "Healthy BMI : "
+        }else{
+            return  "OverWeight BMI : "
         }
     }
 }
